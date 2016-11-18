e25c99ee3d8b8da314d8198e857f5573482a6b5c
gdmec07150832 <709681631@qq.com>
2016-11-18 17:16:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wolf/mediaplayerservice/PlayService.java
@@ -0,0 +1,39 @@
+package com.example.wolf.mediaplayerservice;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+
+/**
+ * Created by wolf on 2016/11/18.
+ */
+public class PlayService extends Service{
+    MediaPlayer mediaPlayer;
+    public PlayService(){
+    }
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer=MediaPlayer.create(this,R.raw.exodus);
+        mediaPlayer.setLooping(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
+}
